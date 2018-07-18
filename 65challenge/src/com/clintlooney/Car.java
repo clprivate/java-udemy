efdbc641b3d78a528ca5cd0130c384511d0d1a21
Looney <clooney@amazon.com>
2018-07-18 09:51:50 -0700

    Modify 65challenge

--- 65challenge/src/com/clintlooney/Car.java
+++ 65challenge/src/com/clintlooney/Car.java
@@ -1,12 +1,43 @@
 package com.clintlooney;
 
 public class Car extends Vehicle {
-    private int maxGear;
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManual;
     private int currentGear;
 
-    public Car (int maxGear,int currentGear) {
-        this.maxGear = maxGear;
-        this.currentGear = currentGear;
+    public Car(String name, String size, int wheels,int doors, int gears, boolean isManual) {
+        super(name,size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1; // This was removed from the parameters because we wish to set is as 1 rather than taking input
     }
+
+    // Change gear
+    public void shiftGears(int currentGear) {
+        if(currentGear > gears) {
+            System.out.println("Invalid gear entered");
+        } else {
+            this.currentGear = currentGear;
+            System.out.println("Car.setCurrentGear -- changed to gear: " + currentGear);
+        }
+    }
+
+    // Change velocity
+    public void changeVelocity(int speed, int direction) {
+        move(speed, direction);
+        System.out.println("Car.changeVelocity() called - Direction = " + direction + ", speed = " + speed);
+    }
+
+    // stop
+    @Override
+    public void stop() {
+        super.stop();
+    }
+
+    // Steer
 
 }
--- 65challenge/src/com/clintlooney/Vehicle.java
+++ 65challenge/src/com/clintlooney/Vehicle.java
@@ -1,11 +1,49 @@
 package com.clintlooney;
 
 public class Vehicle {
-    private int maxSpeed;
-    private int currentSpeed;
-    private int direction;
+    // State
+    private String name;
+    private String size;
 
-    public void changeSpeed(int currentSpeed, int maxSpeed, int speedChange) {
+    int currentVelocity;
+    int currentDirection;
 
+    // Behavior
+    public void steer(int direction) {
+        this.currentDirection += direction;
+        System.out.println("Vehicle.steer(): steering to " + currentDirection + " degrees.");
+    }
+
+    public void move(int velocity, int currentDirection) {
+        System.out.println("Vehicle.move(): Moving toward " + currentDirection + " degrees at " + currentVelocity + " degrees.");
+    }
+
+    public void stop() {
+        this.currentVelocity = 0;
+        System.out.println("Vehicle.stop() called - speed reduced t0 " + currentVelocity);
+    }
+    // Constructor
+    public Vehicle(String name,String size) {
+        this.name = name;
+        this.size = size;
+        this.currentVelocity = 0;
+        this.currentDirection = 0;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+
+    public int getCurrentVelocity() {
+        return currentVelocity;
     }
 }
